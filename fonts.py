f25f4d6ac5330fa5cbd5950a07d48de7064d99e6
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-06-24 11:48:04 +0600

    Changing Bangla font to SolaimanLipi

--- fonts.py
+++ fonts.py
@@ -3,7 +3,7 @@ def get_font_name(langugae):
     # This function will return the font name depending on the language
 
     fonts = {
-                'Bangla' : 'Siyam Rupali 10',
+                'Bangla' : 'SolaimanLipi 10',
             }
 
     if font in fonts:
