d7498529ce41a04d358b2dc0835bbaa2e8d14818
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-06-25 15:09:19 +0600

    Adding some languages with fonts

--- fonts.py
+++ fonts.py
@@ -3,7 +3,20 @@ def get_font_name(language):
     # This function will return the font name depending on the language
 
     fonts = {
+                'Arabic' : 'Lateef 10',
                 'Bangla' : 'SolaimanLipi 10',
+                'Chinese' : 'MandarinFont 10',
+                'Finnish' : '',
+                'French' : '',
+                'German' : '',
+                'Greek' : '',
+                'Japanese' : '',
+                'Korean' : '',
+                'Russian' : '',
+                'Spanish' : '',
+                'Swedish' : '',
+                'Ukrainian' : '',
+                'Urdu' : '',
             }
 
     if language in fonts:
